a7fca454c81a99619831defaaa904de0ad42a28c
OnceAShadow <fred.sevillano@gmail.com>
2017-10-14 02:35:44 -0400

    Clean

--- Every_Beginning/Every_Beginning/src/com/fap/Every_Beginning/Game.java
+++ Every_Beginning/Every_Beginning/src/com/fap/Every_Beginning/Game.java
@@ -7,42 +7,35 @@ import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
-
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 
-	private static int WidthResolution = 100;
-	private static int HeightResolution = 100;
-	
+	private static int WidthResolution = 800;
+	private static int HeightResolution = 600;
 	private static int WidthFrame = WidthResolution;
 	private static int HeightFrame = HeightResolution;
 	
 	public static String Title = "Every Beginning";
-	
-	
+
 	private Thread thread;
 	private JFrame Frame;
-	
-	
+
 	private boolean Running;
 	
 	private BufferedImage ImageInFrame = new BufferedImage(WidthFrame, HeightFrame, BufferedImage.TYPE_INT_RGB);
 	private int[] PixelsInFrame = ((DataBufferInt) ImageInFrame.getRaster().getDataBuffer()).getData();
 
-	
 	public Game () {
 		Dimension FrameResolution = new Dimension(WidthFrame, HeightFrame);
 		setPreferredSize(FrameResolution);
 		
 		Frame = new JFrame();
-		
-
+		Frame.setResizable(false);
 	}
 	
 	public static void main(String[] args) {
 		Game game = new Game();
-		game.Frame.setResizable(false);
 		game.Frame.setTitle(game.Title);
 		game.Frame.add(game);
 		game.Frame.pack();
@@ -51,30 +44,27 @@ public class Game extends Canvas implements Runnable {
 		game.Frame.setVisible(true);
 
 		game.Start();
-
 	}
 	
 	public synchronized void Start() {
 		Running = true;
 		thread = new Thread(this, "Affichage");
 		thread.start();
-				
 	}
 	
 	public void run() {
 		long Clock1Sec = System.currentTimeMillis();
-
-		
 		requestFocus();
 		
-		int FPS = 0, TPS = 0;
+		int FPS = 0;
+		int TPS = 0;
 		
 		while(Running) {
 			
 			TickGame();
-			TPS++;
-			
 			RenderOnScreen();
+
+			TPS++;
 			FPS++;
 			
 			if (System.currentTimeMillis() - Clock1Sec > 1000) {
@@ -86,12 +76,11 @@ public class Game extends Canvas implements Runnable {
 				
 			}
 		}
+
 		Stop();
 	}
-	
-	
+
 	public void TickGame() {
-		
 	}
 	
 	public void RenderOnScreen() {
@@ -102,16 +91,12 @@ public class Game extends Canvas implements Runnable {
 		}
 		
 		Graphics graphics = BufferStrategy.getDrawGraphics();
-
 		graphics.setColor(new Color(0xFF00FF));
 		graphics.fillRect(0, 0, WidthFrame, HeightFrame);
-	
 		graphics.drawImage(ImageInFrame, 0, 0, WidthResolution, HeightResolution, null);
-
-		
 		graphics.dispose();
+
 		BufferStrategy.show();
-	
 	}
 	
 	public synchronized void Stop() {
@@ -121,8 +106,5 @@ public class Game extends Canvas implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
-	
 }
