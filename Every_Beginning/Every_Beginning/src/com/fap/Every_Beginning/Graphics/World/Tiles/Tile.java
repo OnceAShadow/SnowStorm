61ff0203f39addfb6bad1d38b18ad79c4690872c
OnceAShadow <fred.sevillano@gmail.com>
2017-10-15 23:21:48 -0400

    tile

--- Every_Beginning/Every_Beginning/src/com/fap/Every_Beginning/Graphics/World/Tiles/Tile.java
+++ Every_Beginning/Every_Beginning/src/com/fap/Every_Beginning/Graphics/World/Tiles/Tile.java
@@ -1,6 +1,11 @@
 package com.fap.Every_Beginning.Graphics.World.Tiles;
 
+import com.fap.Every_Beginning.Core.GameOn;
 import com.fap.Every_Beginning.Graphics.Animations.Sprite;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 public class Tile {
 
@@ -9,14 +14,21 @@ public class Tile {
     public Sprite sprite;
     public int phySpec;
 
+    BufferedImage imageInTile;
+    int[] pixelsInTile;
+    Graphics graphics;
 
-
-    
     public Tile(Sprite sprite) {
         this.sprite = sprite;
+        imageInTile = new BufferedImage(sprite.widthSprite, sprite.heightSprite, BufferedImage.TYPE_INT_RGB);
+        pixelsInTile = ((DataBufferInt) imageInTile.getRaster().getDataBuffer()).getData();
     }
     
     public int getPhySpec() {
     	return phySpec;
+    }
+
+    public void drawTile(int x, int y, GameOn game) {
+        imageInTile.getGraphics().drawImage(imageInTile, x * sprite.widthSprite, y * sprite.heightSprite, sprite.widthSprite,  sprite.heightSprite,  null);
     }
 }
